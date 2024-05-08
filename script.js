46ed73decfdc6fe342bff738e47a7dd025257857
notdule <148584497+notdule@users.noreply.github.com>
2024-05-08 23:52:45 +0200

    Final Code Update!

--- script.js
+++ script.js
@@ -17,5 +17,12 @@ dateOptionsSelectElement.addEventListener("change", () => {
       currentDateParagraph.textContent = formattedDate
         .split("-")
         .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
+    default: 
+     currentDateParagraph.textConten = formattedDate;
   }
-});
+   });
